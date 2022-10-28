323d244e612cb7d3267d9d9a75d944bf47ec72ea
LokeshD12345 <116729096+LokeshD12345@users.noreply.github.com>
2022-10-28 11:01:17 +0530

    Add files via upload

---
+++ Tasks.java
@@ -0,0 +1,17 @@
+class Tasks {
+    public static void main(String[] args) {
+
+        String a = "Lokesh";      //Literal
+        System.out.println(a.length());
+
+        String s1=new String(a);    //object 
+        System.out.println(s1.length());
+
+        int i=a.length();
+        System.out.println(i);
+
+
+
+
+    }
+}
